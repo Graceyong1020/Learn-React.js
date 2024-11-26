5e8a5d76971ff76e476381451df7669861761b77
GRACE\Baby <graceyong1020@gmail.com>
2024-11-26 07:07:22 -0800

    node

---
+++ learn_react/section03/src/index.js
@@ -0,0 +1,18 @@
+//import {add, sub} from "./math.js"; // ES module
+//import multiply from "./math.js";
+import multiply, {add, sub} from "./math.js";
+// const {add, sub} = require("./math"); // commonJS module
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+//console.log(add(1, 2));
+//console.log(sub(1, 2));
+//console.log(multiply(4, 2));
+
+//console.log(moduleData);
+
+
+
---
+++ learn_react/section03/src/math.js
@@ -0,0 +1,21 @@
+// math module
+
+export function add(a,b) {
+    return a + b;
+}
+
+export function sub(a,b) {
+    return a - b;
+}
+
+export default function multiply(a,b) {
+    return a * b;
+}
+
+// commonJS module
+//module.exports = {
+  //  add,
+   // sub,
+//}
+
+// ES6 module -> function 앞에 export를 붙여준다.

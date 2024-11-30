bdb8cf9b51fbf66da305da6349056062d55702b0
GRACE\Baby <graceyong1020@gmail.com>
2024-11-29 21:52:18 -0800

    chapter 5 - complete

--- learn_react/section05/src/App.jsx
+++ learn_react/section05/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { useState } from "react"; // useState를 사용하기 위해 import 해줌
 
 import Register from "./components/Register";
+import HookExam from "./components/HookExam";
 
 function App() {
   return (
     <>
-      <Register />
+      {/* <Register /> */}
+      <HookExam />
     </>
   );
 }
